ea0b1d63ab915a612cce0b74e95a52da6e9b4b55
SerhiyPro <SergProts121314>
2019-10-05 19:16:51 +0300

    Create Comment model

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -3,3 +3,4 @@ from server.models.users import Users
 from server.models.revoked_token import RevokedTokenModel
 from server.models.work_spheres import WorkSpheres
 from server.models.portfolio import Portfolio
+from server.models.comment import Comment
