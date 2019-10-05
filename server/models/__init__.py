974c91da75b858a04958528486aa1d21186b8ee7
SerhiyPro <SergProts121314>
2019-10-05 18:47:41 +0300

    Create WorkSpheres model

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -1,3 +1,4 @@
 from server.models.services import Services
 from server.models.users import Users
 from server.models.revoked_token import RevokedTokenModel
+from server.models.work_spheres import WorkSpheres
