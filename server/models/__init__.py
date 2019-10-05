23b8caab615bb9a5d71fd021b7649321ebf4deb8
SerhiyPro <SergProts121314>
2019-10-05 19:25:37 +0300

    Create Appointment model

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -4,3 +4,4 @@ from server.models.revoked_token import RevokedTokenModel
 from server.models.work_spheres import WorkSpheres
 from server.models.portfolio import Portfolio
 from server.models.comment import Comment
+from server.models.appointment import Appointment
