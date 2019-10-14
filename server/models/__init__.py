3efe0e1e951e7629f7c8798b6f0c64233e267f19
SerhiyPro <SergProts121314>
2019-10-14 17:43:05 +0300

    Create Comment controllers

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -3,5 +3,5 @@ from server.models.users import Users
 from server.models.revoked_token import RevokedTokenModel
 from server.models.work_spheres import WorkSpheres
 from server.models.portfolio import Portfolio
-from server.models.comment import Comment
+from server.models.comments import Comments
 from server.models.appointment import Appointment
--- server/models/comments.py
+++ server/models/comments.py
@@ -1,7 +1,7 @@
 from server import db
 
 
-class Comment(db.Model):
+class Comments(db.Model):
     __tablename__ = 'comment'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -13,6 +13,23 @@ class Comment(db.Model):
     def __repr__(self) -> str:
         return f'Comment # {self.id}, by - {self.commentators_name}'
 
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def get_self_representation(self):
+        return {
+            'id': self.id,
+            'commentators_name': self.commentators_name,
+            'value': self.value,
+            'rate': self.rate,
+            'is_checked': self.is_checked
+        }
+
+    def update(self, key, item):
+        setattr(self, key, item)
+        db.session.commit()
+
     def save_to_db(self):
         db.session.add(self)
         db.session.commit()
@@ -20,3 +37,7 @@ class Comment(db.Model):
     @classmethod
     def get_by_id(cls, _id: int):
         return cls.query.filter_by(id=_id).first()
+
+    @classmethod
+    def return_all(cls):
+        return cls.query.all()
