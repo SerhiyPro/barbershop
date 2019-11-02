71cfeca4250ff16425324af77e2fd34fde885573
SerhiyPro <SergProts121314>
2019-11-02 14:49:10 +0200

    Add check of overlapping procedures time, display procedures time of given barber

--- server/controllers/appointments.py
+++ server/controllers/appointments.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from flask_jwt_extended import jwt_required
 from flask_restful import Resource, reqparse
 from server import api
@@ -9,33 +9,42 @@ parser = reqparse.RequestParser()  # Adding request parses
 
 
 class AppointmentsAll(Resource):
-    # @jwt_required   TODO: uncomment
     def get(self):
-        # Only for history and testing
-        return {'appointments': list(map(lambda x: x.get_self_representation(), Appointments.return_all()))}, 200
+        return {'appointments': list(map(lambda x: x.get_self_representation(), Appointments.get_all()))}, 200
 
-    def post(self, ):
-        parser.add_argument('clients_name', help='This field cannot be blank', required=True)
-        parser.add_argument('clients_phone_number', help='This field cannot be blank', required=True)
-        parser.add_argument('service', type=int, help='This field cannot be blank and should be integer', required=True)
-        parser.add_argument('barber', type=int, help='This field cannot be blank and should be integer', required=True)
-        parser.add_argument('procedure_start_datetime', help='This field cannot be blank', required=True)
-        parser.add_argument('procedure_end_datetime', help='This field cannot be blank', required=True)
-        parser.add_argument('comment', required=False)
-        data = parser.parse_args()
-
+    def post(self):
+        data = get_parser_info()
         try:
-            procedure_start_datetime = datetime.strptime(data['procedure_start_datetime'], '%Y-%m-%d %H:%M:%S')
-            procedure_end_datetime = datetime.strptime(data['procedure_end_datetime'], '%Y-%m-%d %H:%M:%S')
+            procedure_start_datetime = datetime.strptime(data['procedure_start_datetime'], '%Y-%m-%d %H:%M')
+            procedure_end_datetime = datetime.strptime(data['procedure_end_datetime'], '%Y-%m-%d %H:%M')
         except ValueError:
-            return {'message': 'incorrect datetime format'}, 400
+            return {'message': 'Incorrect datetime format'}, 400
+
+        if procedure_start_datetime > procedure_end_datetime:
+            return {'message': 'Incorrect datetime input, start is bigger than end'}, 400
+
+        service = Services.get_by_name_or_id(id=data['service'])
+        if not service:
+            return {'message': f'Service with id {data["service"]} does not exist'}, 400
+
+        barber = Users.get_by_username_or_id(id=data['barber'])
+        if not barber:
+            return {'message': f'User with id {data["barber"]} does not exist'}, 400
+        barber_appointments = Appointments.get_users_appointments(user_id=barber.id)
+        procedure_day = procedure_start_datetime.date().strftime("%Y-%m-%d")
+        if procedure_day in barber_appointments:
+            for appointment in barber_appointments[procedure_day]:
+                appointment_start = datetime.strptime(appointment['procedure_start_datetime'], '%Y-%m-%d %H:%M')
+                appointment_end = datetime.strptime(appointment['procedure_end_datetime'], '%Y-%m-%d %H:%M')
+                if procedure_start_datetime in get_datetime_range(start=appointment_start, end=appointment_end):
+                    return {'message': 'Please choose another time for procedure'}, 400
 
         if data['comment']:
             new_appointment = Appointments(
                 clients_name=data['clients_name'],
                 clients_phone_number=data['clients_phone_number'],
-                service=data['service'],  # TODO: check if exists
-                barber=data['barber'],
+                service=service.id,
+                barber=barber.id,
                 procedure_start_datetime=procedure_start_datetime,
                 procedure_end_datetime=procedure_end_datetime,
                 comment=data['comment']
@@ -57,40 +66,24 @@ class AppointmentsAll(Resource):
         }, 201
 
 
+class UserAppointments(Resource):
+    def get(self, user_id):
+        user = Users.get_by_username_or_id(id=user_id)
+        if not user:
+            return {'message': f'User with id {user_id} does not exist'}, 400
+
+        return {'appointments': Appointments.get_users_appointments(user_id=user.id)}, 200
+
+
 class Appointment(Resource):
     def get(self, id):
+        appointment = Appointments.get_by_id(id)
+        if not appointment:
+            return {'message': f'Appointment with id {id} does not exist'}, 400
 
-        comment = Comments.get_by_id(id)
-        if not comment:
-            return {'message': f'Comment with id {id} does not exist'}, 400
-
-        return {'comment': comment.get_self_representation()}, 200
+        return {'appointment': appointment.get_self_representation()}, 200
 
     @jwt_required
-    def put(self, id):
-        parser.add_argument('commentators_name', required=False)
-        parser.add_argument('value', required=False)
-        parser.add_argument('rate', type=int, help='This field should be integer', required=False)
-        parser.add_argument('is_checked', required=False)
-        data = parser.parse_args()
-
-        comment = Comments.get_by_id(id)
-        if not comment:
-            return {'message': f'Comment with id {id} does not exist'}, 400
-
-        for key, value in data.items():
-            if not value:
-                continue
-            if key == 'is_checked':
-                value = True if value in ('true', 'True', 1) else False
-            comment.update(key, value)
-
-        return {
-                   'message': 'Comment was successfully updated',
-                   'comment': comment.get_self_representation()
-        }, 200
-
-    # @jwt_required  TODO: uncomment
     def delete(self, id):
         appointment = Appointments.get_by_id(id)
         if not appointment:
@@ -100,6 +93,28 @@ class Appointment(Resource):
         return {}, 202
 
 
+def get_datetime_range(start=None, end=None):
+    minutes = []
+    while start < end:
+        minutes.append(start)
+        start += timedelta(minutes=1)
+    return minutes
+
+
+def get_parser_info():
+    parser.add_argument('clients_name', help='This field cannot be blank', required=True)
+    parser.add_argument('clients_phone_number', help='This field cannot be blank', required=True)
+    parser.add_argument('service', type=int, help='This field cannot be blank and should be integer', required=True)
+    parser.add_argument('barber', type=int, help='This field cannot be blank and should be integer', required=True)
+    parser.add_argument('procedure_start_datetime', help='This field cannot be blank', required=True)
+    parser.add_argument('procedure_end_datetime', help='This field cannot be blank', required=True)
+    parser.add_argument('comment', required=False)
+    appointment_data = parser.parse_args()
+    for arg in parser.args:
+        parser.remove_argument(arg)
+    return appointment_data
+
+
 api.add_resource(AppointmentsAll, '/api/appointments', methods=['GET', 'POST'])
-# api.add_resource(AppointmentsAll, '/api/comments/<string:user>', methods=['GET', 'POST'])
+api.add_resource(UserAppointments, '/api/appointments/user/<user_id>', methods=['GET'])
 api.add_resource(Appointment, '/api/appointments/<id>', methods=['GET', 'PUT', 'DELETE'])
